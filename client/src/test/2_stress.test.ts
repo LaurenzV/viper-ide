4e9f7372bace37e08c72b5f3db73a2bdd8e1e28b
Laurenz Stampfl <laurenz.stampfl+github@gmail.com>
2024-11-28 18:14:00 +0100

    add print statements

--- client/src/test/2_stress.test.ts
+++ client/src/test/2_stress.test.ts
@@ -14,16 +14,23 @@ suite('ViperIDE Stress Tests', () => {
     test("1. multiple fast verification requests", async function() {
         this.timeout(15000);
 
+        console.log("1. DEBUG!");
         TestHelper.resetErrors();
+        console.log("2. DEBUG!");
         // 1 verification is expected, there should be no subsequent ones
         const verified = TestHelper.waitForVerification(SIMPLE)
             .then(() => TestHelper.waitForTimeout(5000, TestHelper.waitForVerification(SIMPLE)));
+        console.log("3. DEBUG!");
         await TestHelper.openFile(SIMPLE);
+        console.log("4. DEBUG!");
         //submit 10 verification requests
         for (let i = 0; i < 10; i++) {
+            console.log("5. DEBUG! " + i);
             await TestHelper.verify();
         }
+        console.log("6. DEBUG!");
         const timeout = await verified;
+        console.log("7. DEBUG!");
         assert(timeout, "multiple verifications seen");
     });
 
