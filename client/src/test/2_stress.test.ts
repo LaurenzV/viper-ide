deb590599bd4f9994b35e1632b2062dca6ba424c
Laurenz Stampfl <laurenz.stampfl+github@gmail.com>
2024-11-28 18:24:31 +0100

    print better times

--- client/src/test/2_stress.test.ts
+++ client/src/test/2_stress.test.ts
@@ -1,5 +1,6 @@
 import assert from 'assert';
 import TestHelper, { CARBON, SETUP_TIMEOUT, SILICON, SIMPLE } from './TestHelper';
+const { performance } = require('perf_hooks');
 
 suite('ViperIDE Stress Tests', () => {
     suiteSetup(async function() {
@@ -13,24 +14,32 @@ suite('ViperIDE Stress Tests', () => {
 
     test("1. multiple fast verification requests", async function() {
         this.timeout(15000);
+        const startTime = performance.now()
+
+        let printTime = () => console.log("Passed time: ", (performance.now() - startTime), "ms");
 
         console.log("1. DEBUG!");
+        printTime();
         TestHelper.resetErrors();
-        console.log("2. DEBUG!");
         // 1 verification is expected, there should be no subsequent ones
         const verified = TestHelper.waitForVerification(SIMPLE)
             .then(() => TestHelper.waitForTimeout(5000, TestHelper.waitForVerification(SIMPLE)));
         console.log("3. DEBUG!");
+        printTime();
         await TestHelper.openFile(SIMPLE);
         console.log("4. DEBUG!");
+        printTime();
         //submit 10 verification requests
         for (let i = 0; i < 10; i++) {
             console.log("5. DEBUG! " + i);
+            printTime();
             await TestHelper.verify();
         }
         console.log("6. DEBUG!");
+        printTime();
         const timeout = await verified;
         console.log("7. DEBUG!");
+        printTime();
         assert(timeout, "multiple verifications seen");
     });
 
